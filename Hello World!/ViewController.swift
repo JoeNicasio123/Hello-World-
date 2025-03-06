22210cac8878782b075a4c41c6362768cffeb57b
JoeNicasio123 <joenicasio123@gmail.com>
2025-03-06 13:29:42 -0600

    app now display Hello message

--- Hello World!/ViewController.swift
+++ Hello World!/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBOutlet weak var lblOutput: UILabel!
+    
+    @IBAction func showOutput(_ sender: Any) {
+        let name = txtName.text
+        let output = "Hello " + name!
+        lblOutput.text = output
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
