46c208747d2777bef8ed799d8e8c76429e52a838
JoeNicasio123 <joenicasio123@gmail.com>
2025-03-06 13:43:42 -0600

    app now hides keyboard

--- Hello World!/ViewController.swift
+++ Hello World!/ViewController.swift
@@ -22,6 +22,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                                 action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
 }
 
